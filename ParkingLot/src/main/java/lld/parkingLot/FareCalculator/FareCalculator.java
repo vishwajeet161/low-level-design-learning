a7177ff7089b21f902d58816dce221a75e8d828f
vishwajeet161 <vishwajeet.patel161@gmail.com>
2026-06-22 23:13:40 +0530

    fare calculation completed

--- ParkingLot/src/main/java/lld/parkingLot/FareCalculator/FareCalculator.java
+++ ParkingLot/src/main/java/lld/parkingLot/FareCalculator/FareCalculator.java
@@ -1,5 +1,23 @@
 package lld.parkingLot.FareCalculator;
 
-public class FareCalculator {
+import java.math.BigDecimal;
+import java.util.List;
 
+import lld.parkingLot.Ticket;
+import lld.parkingLot.FareCalculator.FareStrategy.FareStrategy;
+
+public class FareCalculator {
+    private List<FareStrategy> fareStrategies;
+
+    public FareCalculator(List<FareStrategy> fareStrategies) {
+        this.fareStrategies = fareStrategies;
+    }
+
+    public BigDecimal calculateFare(Ticket ticket) {
+        BigDecimal fare = BigDecimal.ZERO;
+        for (FareStrategy strategy : fareStrategies) {
+            fare = strategy.CalculateFare(ticket, fare);
+        }
+        return fare;
+    }
 }
