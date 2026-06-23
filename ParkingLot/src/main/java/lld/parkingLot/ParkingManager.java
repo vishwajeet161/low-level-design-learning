6612bc2f39f4d62fd225c6382973985db8560765
vishwajeet161 <vishwajeet.patel161@gmail.com>
2026-06-23 19:49:02 +0530

    findSpotForVehicle completed

--- ParkingLot/src/main/java/lld/parkingLot/ParkingManager.java
+++ ParkingLot/src/main/java/lld/parkingLot/ParkingManager.java
@@ -1,5 +1,35 @@
 package lld.parkingLot;
 
+import lld.parkingLot.Vehicle.VehicleSize;
+import lld.parkingLot.ParkingSpot.ParkingSpot;
+import lld.parkingLot.Vehicle.Vehicle;
+
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
 public class ParkingManager {
-    
+    private final Map<VehicleSize, List<ParkingSpot>> availableSpots;
+    private final Map<Vehicle, ParkingSpot> vehicletoSpotMap;
+
+    public ParkingManager(Map<VehicleSize, List<ParkingSpot>> availableSpots) {
+        this.availableSpots = availableSpots;
+        this.vehicletoSpotMap = new HashMap<>();
+    }
+
+    public ParkingSpot findSpotForVehicle(Vehicle vehicle){
+        VehicleSize vehicleSize = vehicle.getSize();
+
+        for(VehicleSize size: VehicleSize.values()){
+            if(size.ordinal()>= vehicleSize.ordinal()){
+                List<ParkingSpot> spots = availableSpots.get(size);
+                for(ParkingSpot spot: spots){
+                    if(spot.isAvailable()){
+                        return spot;
+                    }
+                }
+            }
+        }
+        return null;
+    }
 }
